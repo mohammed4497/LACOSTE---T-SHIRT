954c8818e8c7c3ea50625f01646a5b09dde24a6d
mohammed4497 <mohammedbarakat1911@gmail.com>
2023-04-15 19:04:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LACOSTE---T-SHIRT
---
+++ main.js
@@ -0,0 +1,12 @@
+let img = document.getElementById('main-img')
+let container = document.querySelector('.container')
+function t_shirts(x)
+{
+
+   img.src = x; 
+}
+function colors(color)
+{
+container.style.background = color;
+
+}
